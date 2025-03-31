946f7d4a562eb9d239ae6b27da53d96c42f21f22
Fedor <feda.vert@gmail.com>
2025-03-31 18:05:54 +0300

    Проверка знаний

--- Практические задания/17_2.py
+++ Практические задания/17_2.py
@@ -1,6 +1,6 @@
-lst = [int(x) for x in open('C:/Users/User/Desktop/InformatikaREP/Практические задания/17_2.txt')]
+# lst = [int(x) for x in open('C:/Users/User/Desktop/InformatikaREP/Практические задания/17_2.txt')]
 
-box = []
-for i in range(len(lst) - 1):
-    for j in range(i + 1, len(lst)):
+# box = []
+# for i in range(len(lst) - 1):
+#     for j in range(i + 1, len(lst)):
         
