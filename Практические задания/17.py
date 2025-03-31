cc45e3590ac49646d43249507d12ea95db53baaa
Fedor <feda.vert@gmail.com>
2025-03-31 18:04:16 +0300

    Проверка знаний

--- Практические задания/17.py
+++ Практические задания/17.py
@@ -1,6 +1,11 @@
 
-al = [int(x) for x in open('17.txt')]
+al = [int(x) for x in open('C:/Users/User/Desktop/InformatikaREP/Практические задания/17.txt')]
 
+box = []
 for i in range(len(al) - 1):
     a, b = al[i:i+2]
-    print(a, b) ; break
+    
+    if (a % 3 == 0) or (b % 3 == 0):
+        box.append(a+b)
+
+print(len(box), max(box))
